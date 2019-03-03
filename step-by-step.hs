70fa320735a746cadabb932908130b5db680d1f7
Tom Busby <tom@busby.ninja>
2019-03-03 15:19:35 +0100

    Replace Identity with IO at the base of the stack

--- step-by-step.hs
+++ step-by-step.hs
@@ -200,7 +200,7 @@ eval4 (App e1 e2) = do
 -- Providing logging capabilities with WriterT
 
 type Eval5 α =
-    ReaderT Env(ExceptT String (WriterT [String] (StateT Integer Identity))) α
+    ReaderT Env (ExceptT String (WriterT [String] (StateT Integer Identity))) α
 
 runEval5 :: Env -> Integer -> Eval5 α -> ((Either String α, [String]), Integer)
 runEval5 env st ev =
@@ -234,5 +234,47 @@ eval5 (App e1 e2) = do
     val2 <- eval5 e2
     case val1 of
         FunVal env' n body ->
-            local (const (Map.insert n val2 env')) (eval5 body)
+            local (const $ Map.insert n val2 env') (eval5 body)
+        _ -> throwError "type error in application"
+
+-- Replace Identity at the base of the stack with IO:
+
+type Eval6 α =
+    ReaderT Env (ExceptT String (WriterT [String] (StateT Integer IO))) α
+
+runEval6 :: Env -> Integer -> Eval6 α
+         -> IO ((Either String α, [String]), Integer)
+runEval6 env st ev = runStateT (runWriterT (runExceptT (runReaderT ev env))) st
+
+eval6 :: Exp -> Eval6 Value
+eval6 (Lit i) = do
+    tick
+    liftIO $ print i
+    return $ IntVal i
+eval6 (Var n) = do
+    tick
+    tell [n]
+    env <- ask
+    case Map.lookup n env of
+        Nothing -> throwError ("unbound variable: " ++ n)
+        Just val -> return val
+eval6 (Plus e1 e2) = do
+    tick
+    e1' <- eval6 e1
+    e2' <- eval6 e2
+    case (e1', e2') of
+        (IntVal i1, IntVal i2) ->
+            return $ IntVal (i1 + i2)
+        _ -> throwError "type error in addition"
+eval6 (Abs n e) = do
+    tick
+    env <- ask
+    return $ FunVal env n e
+eval6 (App e1 e2) = do
+    tick
+    val1 <- eval6 e1
+    val2 <- eval6 e2
+    case val1 of
+        FunVal env' n body ->
+            local (const $ Map.insert n val2 env') (eval6 body)
         _ -> throwError "type error in application"
